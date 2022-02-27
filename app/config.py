b1cb1fe1708071db791f481dccfcb0d3a041400d
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 15:05:08 +0300

    Add the news api url to the config file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     general configuration class
     '''
-    pass
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
 
 class ProdConfig(Config):
     '''
