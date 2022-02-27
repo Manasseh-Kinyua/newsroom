76cc0026e62e658fe087b5460a6533cbc94f8f7a
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 16:15:18 +0300

    Add the format method to the base url in order to add the api key

--- app/request.py
+++ app/request.py
@@ -10,6 +10,12 @@ api_key = app.config['NEWS_API_KEY']
 #getting the news base url
 base_url = app.config['NEWS_API_BASE_URL']
 
+def get_news():
+    '''
+    function to get the json response to our url request
+    '''
+    get_news_url = base_url.format(api_key)
+
 
 
 
