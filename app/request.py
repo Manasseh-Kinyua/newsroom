f7889cb73ded76ca9951d3ec2a226e05008fd884
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 16:07:19 +0300

    Add urllib and json modules to the request file and retrieve the base url form the config file

--- app/request.py
+++ app/request.py
@@ -1,7 +1,14 @@
 from app import app
+import urllib.request,json
+from .models import movie
+
+Movie = movie.Movie
 
 #getting the api key
 api_key = app.config['NEWS_API_KEY']
+
+#getting the news base url
+base_url = app.config['NEWS_API_BASE_URL']
 
 
 
