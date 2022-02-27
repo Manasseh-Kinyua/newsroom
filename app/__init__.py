37a86c3430a8f62d1fcbb422a1207f60ff65ee7a
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 14:31:54 +0300

    Add the index veiws function to display the base page

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 #initializing the app
 app = Flask(__name__)
 
+from app import views
+
 
 
 
---
+++ app/views.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    returns the index page and its data.
+    '''
+    word = 'Hellor World'
+    return render_template('index.html', message = word)
+
+    @app.route('News')
+    def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#rewtewrw
