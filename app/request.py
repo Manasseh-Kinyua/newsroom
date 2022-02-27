cf94587c091293e7ddcff112899419d6fc49801e
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 16:54:24 +0300

    Add a method to process the found data and append it to a list

--- app/request.py
+++ app/request.py
@@ -29,6 +29,26 @@ def get_news():
     return news_results
 
 def process_results(news_list):
+    '''
+    function to transform the movie result into list objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        author = news_item.get('author')
+        title = news_item.get('title')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        urlToImage = news_item.get('urlToImage')
+        publishedAt = news_item.get('publishedAt')
+        content = news_item.get('content')
+
+        news_object = News(authoe,title,description,url,urlToImage,publishedAt,content)
+        print(news_object)
+        news_results.append(news_object)
+
+
+
+
 
 
 
