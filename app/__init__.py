d473a6733966a235421fb6cb7069fd7d23a2ea6d
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 14:59:25 +0300

    Connect the instance forlder and all its contents to the __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from .config import DevConfig
 
 #initializing the app
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 #setting up the configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
 
