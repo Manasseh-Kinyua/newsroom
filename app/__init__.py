e9687c37db22afcf02ccd94c13ea285fc2662230
Manasseh-Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-02-27 14:41:32 +0300

    set the configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 #initializing the app
 app = Flask(__name__)
+
+#setting up the configuration
+app.config.from_object(DevConfig)
 
 from app import views
 
